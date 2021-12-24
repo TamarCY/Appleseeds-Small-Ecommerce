5ec796697c498f03f5885a4fb64dc0dae97d5836
tamarcy <cohen.tamar@gmail.com>
2021-12-24 08:39:11 +0200

    nav bar disined

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,20 +6,39 @@ import "./Navbar.css"
 class Navbar extends React.Component {
     render() {
         return (
-            <div className="Navbar">
-                <Link to="/">
-                    Home
-                </Link>
-                <Link to="/products">
+            <>
+                <div className="ui secondary pointing  huge menu" id ="Navbar">
+                    <Link to="/" className="item">
+                        <i className="fas fa-home"></i>
+                    </Link>
+                    <Link  to="/products" className="item">
                         Products
-                </Link>
-                <Link to="/cart">
-                        Cart
-                </Link>
-            </div>
+                    </Link>
+                    
+                    <div className="right menu">
+                        <Link  to="/" className="ui item">
+                        <i className="fas fa-shopping-cart"></i>
+                        <span className="Navbar-cart">0</span>
+                        </Link>
+                    </div>
+                </div>
+            </>
         )
-    }
-
+    //     return (
+    //         <div className="Navbar">
+    //             <Link to="/">
+    //                 Home
+    //             </Link>
+    //             <Link to="/products">
+    //                     Products
+    //             </Link>
+    //             <Link to="/cart">
+    //                     Cart
+    //             </Link>
+    //         </div>
+    //     )
+    // }
+        }
 }
 
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
 
 
 class Home extends React.Component{
     
     render(){
         return(
-            <h1>
-                Poster Store
+            <div className="Home">
+            <h1 className="Home-header">
+                Welcome To The Poster Store
             </h1>
+            <h2>
+                Where can find here ugly and not interning posters in random prices
+            </h2>
+            <Link to="/products"><i class="fas fa-door-open"></i></Link>
+            </div>
         )
     }
 
